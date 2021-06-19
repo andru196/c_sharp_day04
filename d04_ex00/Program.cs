fbf9edc7c46367457f42728b370b4c41c63e5f01
andru196 <andru196@icloud.com>
2021-06-19 16:53:40 +0300

    ok

---
+++ d04_ex00/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+void ReadAssemblyInfo(Type type)
+{
+    var ass = type.Assembly;   
+    Console.WriteLine($"Type: {type.FullName}\n" +
+    $"Assembly: {ass.FullName}\n"+
+    $"Based on: {type.BaseType.Name}\n");
+    Console.WriteLine("Fields:");
+    foreach (var field in type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic))
+        Console.WriteLine($"{field.FieldType.FullName}.{field.Name}");
+    Console.WriteLine("Properties:\n");
+    foreach (var prop in type.GetProperties())
+        Console.WriteLine($"{prop.PropertyType.FullName}.{prop.Name}");
+    Console.WriteLine("Methods:\n");
+    foreach (var meth in type.GetMethods())
+        Console.WriteLine($"{meth.ReturnType.Name} {meth.Name}"+
+        $"({string.Join(", ", meth.GetParameters().Select(x=>x.ParameterType.Name + " " + x.Name))})");    
+
+}
+
+
+ReadAssemblyInfo(typeof(Microsoft.AspNetCore.Http.DefaultHttpContext)); 
---
+++ d04_ex01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+var val = new Microsoft.AspNetCore.Http.DefaultHttpContext();
+Console.WriteLine($"Old Response value: {val.Response}");
+ChangePrivateNonStaticFieldValue(val, "_response", null);
+Console.WriteLine($"New Response value: {val.Response}");
+
+void ChangePrivateNonStaticFieldValue(object Entity, string fieldName, object newValue)
+{
+    var type = Entity.GetType();
+    var fieldInfo = type.GetField(fieldName, System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+    if (fieldInfo != null)
+    {
+        if ((newValue == null && !fieldInfo.FieldType.IsValueType) || fieldInfo.FieldType == newValue.GetType())
+        {
+            fieldInfo.SetValue(Entity, newValue);
+        }
+    }
+}
